52521c74676b7ff4c0ec57f55da227f52c1020d0
akshithapalli <88572443+akshithapalli@users.noreply.github.com>
2022-01-31 12:15:57 +0530

    Add files via upload

--- Class2.cs
+++ Class2.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace equal_and___
+namespace oopsconcept
 {
     class Class2
     {
-        static void Main(string[]args)
+        static void Main2(string[]args)
         {
-            sample(6);
-            sample(6, 7);
-            sample(6, 7, 8);
+            string name = "akshitha";
+            string Myname = name;
+            var doubleeql = (name == Myname);
+            var eql = name.Equals(Myname);
+            Console.WriteLine(doubleeql);
+            Console.WriteLine(eql);
+            object nameObj = " akshitha";
+            char[]values= { 'a','k','s','h','i','t','h','a'};
+            object myName = new string(values);
+            var doubleeql2 = (nameObj == myName);
+            var eql2 = nameObj.Equals(myName);
+            Console.WriteLine(doubleeql2);
+            Console.WriteLine(eql2);
+        }
 
-        }
-        public static void sample(int x, int y=10, int z=20)
-        {
-            Console.WriteLine("{0},{1},{2}", x, y, z);
-        }
     }
 }
