13c0b0332851aca8e835d64136abe298fe5efb87
akshithapalli <akshithapalli68@gmail.com>
2022-02-01 09:11:12 +0530

    leapyr added

---
+++ oops concept/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oops_concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
