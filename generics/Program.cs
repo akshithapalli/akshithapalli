1f9c445f7e65207053a7f749d6ca4b497f5de59a
akshithapalli <akshithapalli68@gmail.com>
2022-02-07 10:32:19 +0530

    generic  added

---
+++ generics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+
+        public class sample<T>
+        {
+            private T data;
+            public T value
+            {
+                get
+                {
+                    return this.data;
+                }
+                set
+                {
+                    this.data = value;
+                }
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            sample<string> name = new sample<string>();
+            name.value = "akshitha pinishetty";
+
+            sample<float> version = new sample<float>();
+            version.value = 2.0f;
+            Console.WriteLine(name.value);
+            Console.WriteLine(version.value);
+        }
+    }
+}
