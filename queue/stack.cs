4b2779c4db27217aca49a623a542ab6d6c33f56f
akshithapalli <akshithapalli68@gmail.com>
2022-02-10 17:20:41 +0530

    thread  added

--- queue/stack.cs
+++ queue/stack.cs
@@ -6,5 +6,50 @@ namespace queue
 {
     class stack
     {
+        public static void Main(string[] args)
+        {
+            int size = 2;
+            Stack stack = new Stack(size);
+
+            int val1 = 7,
+                val2 = 1;
+
+            stack.Push(val1);
+            stack.Push(val2);
+
+            val1 = stack.Pop();
+            val2 = stack.Pop();
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+        }
+    }
+
+    public class Stack
+    {
+        private int[] stack;
+        private int position;
+
+        public Stack(int size)
+        {
+            position = 0;
+            stack = new int[size];
+        }
+
+        public void Push(int value)
+        {
+            stack[position] = value;
+            position++;
+        }
+
+        public int Pop()
+        {
+            if (position > 0)
+            {
+                position -= 1;
+                return stack[position];
+            }
+            return 0;
+        }
     }
 }
---
+++ thread/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+
+namespace thread
+{
+    class Program
+    {
+        internal class ExampleofThreadJoin
+        {
+            static void Main(string[] args)
+            {
+                Thread thread = new Thread(MethodJoin);
+                Console.WriteLine(thread.ThreadState);
+                thread.Start();
+                Console.WriteLine(thread.ThreadState);
+                thread.Join();
+                Console.WriteLine(thread.ThreadState);
+                Console.WriteLine("Work completed");
+            }
+
+            static void MethodJoin()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Work in progress");
+                }
+            }
+        }
+    }
+}
