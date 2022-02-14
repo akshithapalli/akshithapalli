28b999074d09791ae2112ec4bc7135918baaf255
akshithapalli <akshithapalli68@gmail.com>
2022-02-14 11:03:50 +0530

    exercise  added

---
+++ exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exercise
+{
+    class Program
+    {
+
+        public static void Main1()
+        {
+            string str;
+            int i, len, vow, cons;
+            Console.Write("\n\nCount total number of vowel or consonant :\n");
+            Console.Write("Enter the string \n ");
+            str = Console.ReadLine();
+            vow = 0;
+            cons = 0;
+            len = str.Length;
+            for (i = 0; i < len; i++)
+            {
+
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                {
+                    vow++;
+                }
+                else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+                {
+                    cons++;
+                }
+            }
+            Console.Write("\nThe total number of vowel in the string is : {0}\n", vow);
+            Console.Write("The total number of consonant in the string is : {0}\n\n", cons);
+        }
+    }
+}
+
--- thread/threadjoining.cs
+++ thread/threadjoining.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+
 
 namespace thread
 {
     class threadjoining
     {
+
+        internal class ExampleofThreadJoin
+        {
+            static void Main(string[] args)
+            {
+                Thread thread = new Thread(MethodJoin);
+                Console.WriteLine(thread.ThreadState);
+                thread.Start();
+                Console.WriteLine(thread.ThreadState);
+                thread.Join();
+                Console.WriteLine(thread.ThreadState);
+                Console.WriteLine("Work completed");
+            }
+
+            static void MethodJoin()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Work in progress");
+                }
+            }
+        }
     }
 }
+
